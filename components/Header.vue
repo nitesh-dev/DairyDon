9538c738ca9295a95707d5ead2b392ae4db2bf69
nitesh-dev <niteshdev547@gmail.com>
2023-08-18 12:33:45 +0530

    header done

---
+++ components/Header.vue
@@ -0,0 +1,50 @@
+<script setup lang='ts'>
+</script>
+<template>
+    <div class="page">
+        <header>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/">About Us</NuxtLink>
+            <NuxtLink to="/">Products</NuxtLink>
+            <div class="logo">
+                <img src="../public/images/logo.png" alt="logo">
+            </div>
+            <NuxtLink to="/">Franchise</NuxtLink>
+            <NuxtLink to="/">Blog</NuxtLink>
+            <NuxtLink to="/">Contact Us</NuxtLink>
+
+        </header>
+    </div>
+</template>
+<style scoped>
+header {
+    height: 45px;
+    background-color: white;
+    margin: 2rem 0;
+    border-radius: 6px;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+}
+
+header a{
+    color: black;
+    transition: all 200ms;
+}
+
+header a:hover{
+    font-weight: bold;
+}
+
+
+header .logo {
+    background-color: white;
+    border-radius: 8px;
+}
+
+header .logo img {
+    height: 50px;
+    display: block;
+    margin: 10px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,31 @@
 <script setup lang='ts'>
 </script>
 <template>
-    <h1>Hello starter</h1>
+    <section class="landing">
+        <div class="content">
+            <Header></Header>
+        </div>
+        <div class="image-holder">
+            <img src="../public/images/outline/Vector 3.png">
+        </div>
+    </section>
 </template>
 <style scoped>
-h1 {
-    color: red;
+
+.landing{
+    display: grid;
+    grid-template-columns: 100%;
 }
+
+.landing .content{
+    background-color: var(--color-primary);
+    min-height: 400px;
+}
+
+
+.landing .image-holder img{
+    width: 100%;
+}
+
+
 </style>
